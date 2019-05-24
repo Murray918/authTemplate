73714f6700bffdeb4836ebd065b79a87a2976565
Andrew Murray <muhr@pop-os.localdomain>
2019-05-24 18:44:24 -0500

    inital commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+
+//this is the users route
+const users = require('./routes/users');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser())
+
+app.use('/api/v1/users', users);
+
+module.exports = app;
