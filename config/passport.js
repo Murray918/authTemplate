b37758f93556320b91b0226b423412f7116204a7
Andrew Murray <darbyguitars@gmail.com>
2019-05-29 10:31:00 -0500

    async verions is working

--- config/passport.js
+++ config/passport.js
@@ -11,40 +11,31 @@ passport.use(
 			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
 			callbackURL: process.env.GOOGLE_CALLBACK
 		},
-		(accessToken, refreshToken, profile, cb) => {
+		async (accessToken, refreshToken, profile, cb) => {
 			// a user has logged in with OAuth...
 			//look to see if we have a user that matches the profile.id
-			User.findOne({ googleId: profile.id }, (error, user) => {
-				//if there is an error execute callback with error
-				if (error) return cb(error)
-				//if mongoose returns a user set the error to null and execute the callback with the     returned User
-				if (user) {
-					return cb(null, user)
+			try {
+				const  user = await User.findOne({ googleId: profile.id })
+				//if the user exists 
+				if (!!user) { 
+				return	cb(null, user) 
 				} else {
-					// we have a new User via OAuth!
 					const newUser = new User({
 						name: profile.displayName,
 						email: profile.emails[0].value,
 						googleId: profile.id
 					})
-					//save the new user if there is no errors
-					newUser
-						.save()
-						//execute the callback with the new User
-						.then(newUser => {
-							console.log('newUser', newUser)
-							cb(null, newUser)
-						})
-						.catch(error => {
-							console.log(error)
-							cb(error)
-						})
+				 await newUser.save()
+					//execute the callback with the new User
+					return cb(null, newUser)
 				}
-			})
-		}
-	)
+			} 
+			catch(error) {
+				cb(error)
+			}
+		})	
 )
-
+	
 //serialize the User
 passport.serializeUser(function(user, done) {
 	done(null, user.id)
