b86aeaa23fc4df3fa093d6baf0d32464dad69efe
Andrew Murray <darbyguitars@gmail.com>
2019-06-10 22:15:02 -0500

    adding the sql queries

--- sql/Users/create.sql
+++ sql/Users/create.sql
@@ -1,9 +1,11 @@
-
 /*
-    Inserts a new user record.
+    Creates table Users.
     NOTE: We only add schema here to demonstrate the ability of class QueryFile
     to pre-format SQL with static formatting parameters when needs to be.
 */
-INSERT INTO ${schema~}.users(name)
-VALUES($1)
-RETURNING *
+
+CREATE TABLE ${schema~}.users
+(
+    id serial PRIMARY KEY,
+    name text NOT NULL
+)
